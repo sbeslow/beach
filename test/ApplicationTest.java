a1d642dc7bbb876d43532a77f4bce080a978c0ec
Scott Beslow <beslow-scott@norc.org>
2015-04-30 20:09:36 -0500

    Minor

--- test/ApplicationTest.java
+++ test/ApplicationTest.java
@@ -1,21 +1,7 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import com.fasterxml.jackson.databind.JsonNode;
 import org.junit.*;
 
 import play.mvc.*;
 import play.test.*;
-import play.data.DynamicForm;
-import play.data.validation.ValidationError;
-import play.data.validation.Constraints.RequiredValidator;
-import play.i18n.Lang;
-import play.libs.F;
-import play.libs.F.*;
-import play.twirl.api.Content;
-
 import static play.test.Helpers.*;
 import static org.fest.assertions.Assertions.*;
 
@@ -29,17 +15,12 @@ import static org.fest.assertions.Assertions.*;
 public class ApplicationTest {
 
     @Test
-    public void simpleCheck() {
-        int a = 1 + 1;
-        assertThat(a).isEqualTo(2);
+    public void testNullName() {
+    	  Result result = callAction(
+    			    controllers.routes.ref.Application.index(),
+    			    new FakeRequest(POST, "/addPerson?teamName=Bears")
+    			  );
+    	  assertThat(status(result)).isEqualTo(play.mvc.Http.Status.BAD_REQUEST);
     }
-
-    @Test
-    public void renderTemplate() {
-        Content html = views.html.index.render("Your new application is ready.");
-        assertThat(contentType(html)).isEqualTo("text/html");
-        assertThat(contentAsString(html)).contains("Your new application is ready.");
-    }
-
 
 }
